e3f26e7876eea728a9ee219b4cc5a886e5b5393d
Mo Roshid <167237482+Muxro2@users.noreply.github.com>
2025-11-21 22:38:58 +0000

    import fix

--- app/homescreen/page.tsx
+++ app/homescreen/page.tsx
@@ -1,11 +1,10 @@
 import Image from "next/image";
 
-import Header from "@/components/Layout/Header"
-import Notification from "@/components/UI/Notification"
-import LevelInfo from '@/components/UI/LevelInfo'
-import IconButton from '@/components/UI/IconButton'
-import Card from '@/components/UI/Card'
-import Button from '@/components/UI/Button'
+import Header from "@/components/layout/Header"
+import Notification from "@/components/ui/Notification"
+import IconButton from '@/components/ui/IconButton'
+import Card from '@/components/ui/Card'
+import Button from '@/components/ui/Button'
 
 import fire from "@/public/fire.png"
 import play from "@/public/play.png"
@@ -15,7 +14,6 @@ export default function Home() {
     <div className="w-full h-full">
       <Header name="Mo" rank="Expert Mixologyst" level={32}/>
       <Notification source={fire} text="You're on fire!! 8 day streak!" />
-      <LevelInfo />
       <IconButton href="/challenge" icon={play} title="Daily Challange" description="Mix challanging cocktails" info="+100XP" />
 
     <div className="w-[90%] mx-auto flex gap-2">
